ae0b1fdaf7aa6eb4e12818f9a4b66f0b4cc79e31
profesorProg <45238068+profesorProg@users.noreply.github.com>
2025-02-28 10:21:32 +0100

    Primer commit

---
+++ E02.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class E02{
+	public static void main(String[] args){
+		Scanner teclado=new Scanner(System.in);
+		int numero1, numero2;
+		String salida;
+	
+		System.out.print( "Introduzca número entero: ");
+		numero1=teclado.nextInt();
+		System.out.print( "Introduzca número entero: ");
+		numero2=teclado.nextInt();
+		
+		if (numero1%numero2==0){
+			salida=String.format("El %d es divisor del %d\n",numero2,numero1);
+		}
+		else{
+			salida=String.format("El %d NO es divisor del %d\n",numero2,numero1);
+		}
+		
+		System.out.println(salida);
+		
+		
+		
+		
+	}
+	
+}
